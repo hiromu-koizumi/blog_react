094b271a24eaa829ad0b4938bfa7f9db643982ba
小泉大夢 <>
2019-08-08 13:22:54 +0900

    fix:action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,22 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash'
+
+//値をapi経由で取得しstoreに保存している
 
 export const fetchPosts =  () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
-          dispatch({type:'FETCH_POSTS',payload: response.data});    
+    dispatch({type:'FETCH_POSTS',payload: response.data}); 
 };
 
 export const fetchUser = id => async dispatch => {
+  _fetchUser(id,dispatch)
+};
+
+//同じユーザーIDは一度だけ取得するようにしている
+const _fetchUser = _.memoize(async (id, dispatch) =>{
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
-    dispatch({type:'FETCh_USER',payload:response.data});
-};
+    dispatch({type:'FETCH_USERS',payload: response.data});  
+})
 
